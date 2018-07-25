c3f3ffbedf3dd3a0f93615457df05573beba5186
micnncim <micnncim@gmail.com>
2018-07-25 18:48:09 +0900

    Add markdown.go

---
+++ markdown.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+type Snippet struct {
+	Filename string
+	Content  string
+}
+
+func CreateSnippets(filename string) []Snippet {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	var snippets []Snippet
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if !strings.HasPrefix(line, "```") {
+			continue
+		}
+		filename := strings.Trim(line, "`")
+		var code []string
+		for scanner.Scan() {
+			line := scanner.Text()
+			if strings.HasPrefix(line, "```") {
+				break
+			}
+			code = append(code, line)
+		}
+
+		for i := range code {
+			if i == len(code)-1 {
+				break
+			}
+			code[i] += "\n"
+		}
+
+		var content string
+		for _, c := range code {
+			content += c
+		}
+		snippets = append(snippets, Snippet{
+			Content:  content,
+			Filename: filename,
+		})
+	}
+
+	return snippets
+}
